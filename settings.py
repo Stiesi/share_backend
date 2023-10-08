cf0f08ee09ed4bfbea2ca027f35793dacc83ce26
Jens J <stiesel_diesel@yahoo.de>
2023-10-08 08:11:47 +0200

    environment in env dir

--- settings.py
+++ settings.py
@@ -6,9 +6,11 @@ import os
 # 
 class Settings(BaseSettings):
     # to work on Deta: path of .env not found
-    model_config = SettingsConfigDict(env_file=os.path.join('.','.env'), env_file_encoding='utf-8')
+    model_config = SettingsConfigDict(env_file=os.path.join('env','env'), env_file_encoding='utf-8')
     #api_key: Union[str,None] = Field(default=None,alias='my_api_key')      
     eurex_margins : Union[str,None] = Field(default=None)#,alias='eurex_api_key')  
     eurex_base : Union[str,None] = Field(default=None)#,alias='deta_api_key')  
 
 settings=Settings()#_env_file='secrets.env' , _env_file_encoding='utf-8')
+print('env-file',os.path.abspath("env"))
+print('path',os.path.curdir)
